944c5b3fcbe2f39ba0f2a3a6341803cf588cb221
Alex Doss <alexdoss@Alexs-MacBook-Pro-5.local>
2019-04-19 10:34:54 -0600

    React set up

---
+++ src/Components/Product/Product.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export default class Product extends Component {
+
+
+  render() {
+    return  (
+      <div>
+        <h1>Product</h1>
+      </div>
+    )
+  }
+}
